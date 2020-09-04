5a26422477559da0005d3e77407189d5bae1a3ea
maisjamil1 <maisjamil17118@gmail.com>
2020-09-05 01:16:24 +0300

    done

--- data-structures-and-algorithms/data_structures_and_algorithms/challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ data-structures-and-algorithms/data_structures_and_algorithms/challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,47 +1,109 @@
-class Cat:
-    def __init__(self,nameC):
-        self.name=nameC
-        self.type='cat'##to do a check in the dequeue part
-        self.next=None
-
-
 
 class Dog:
-    def __init__(self,nameD):
-        self.name=nameD
-        self.type='dog'
-        self.next=None
+    """
+    this class will create a dog node
+    """
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.type = 'dog'
+
+class Cat:
+    """
+    this class will create a cat node
+    """
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.type = 'cat'
 
 
-class AnimalShelter:
-    # The shelter operates using a first-in, first-out approach.
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class AnimalShelter :
+    def __init__(self):
+        self.cats_queue=Queue()
+        self.dogs_queue=Queue()
+
+    # _________________________________________________
+
+    def enqueue(self,animal_name,animal_type):
+        """
+        this method adds a new node to the back of the (cats or dog) queue with an O(1) Time performance
+        """
+        if animal_type.lower() == 'cat':
+            new_animal=Cat(animal_name)
+
+            self.cats_queue.enqueue(new_animal)
+
+        elif animal_type.lower()  == 'dog':
+            new_animal=Dog(animal_name)
+
+            self.dogs_queue.enqueue(new_animal)
+            
+        else :
+            return None
+
+    # _________________________________________________
+
+    def dequeue(self,animal_type):
+        """
+        removes the node from the front of (cats or dog) queue, and returns the node’s value
+        """
+        
+        if animal_type.lower() == 'cat':
+            return self.cats_queue.dequeue()
+
+        elif animal_type.lower() == 'dog':
+            return self.dogs_queue.dequeue()
+        else :
+            return None
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+class Queue:
+    """
+    this class will create a queue
+    """
     def __init__(self):
         self.front = None
         self.rear = None
-    
-    def enqueue(self, animal_name,animal_type):
 
-        if animal_type == 'cat'
-            cat = Cat(animal_name)
-            if not self.front and not self.rear:
-                self.front = cat
-                self.rear = cat
 
-            old_rear = self.rear
-            self.rear = cat
-            old_rear.next = self.rear
+
+    # def enqueue(self, value):
+    def enqueue(self,new_animal):
+        """
+        this method adds a new node to the back of the queue with an O(1) Time performance
+        """
+        node = new_animal
+        if not self.front and not self.rear:
+            self.front = node
+            self.rear = node
+
+        old_rear = self.rear
+        self.rear = node
+        old_rear.next = self.rear
         
-        if animal_type == 'dog'
-            dog = Dog(animal_name)
-            if not self.front and not self.rear:
-                self.front = dog
-                self.rear = dog
-
-            old_rear = self.rear
-            self.rear = dog
-            old_rear.next = self.rear
     # _________________________________________________
-      def __str__(self):
+
+
+    def dequeue(self):
+        """
+        removes the node from the front of the queue, and returns the node’s value
+        """
+        try:
+            first_node = self.front
+            self.front = self.front.next
+            return first_node.value
+        except Exception as err:
+            return "Queue is empty"
+
+
+
+    # _________________________________________________
+    def __str__(self):
         """
         this method creates a readable output for the user
         """
@@ -61,10 +123,14 @@ class AnimalShelter:
 
 
 if __name__ == '__main__':
-    animal_1 = AnimalShelter()
-    animal_1.enqueue(5)
-    enqueue("kera",'cat')
-    enqueue("jojo",'cat')
-    enqueue("Abbas",'dog')
-    print(nums)
-    print(nums.__str__())
+  
+    animmmals=AnimalShelter()
+    animmmals.enqueue('soso','cat')
+    animmmals.enqueue('lolo','cat')
+    animmmals.enqueue('max','dog')
+    animmmals.enqueue('fofo','dog')
+    print(f'cats_queue :{animmmals.cats_queue}')
+    print(f'dogs_queue :{animmmals.dogs_queue}')
+    print(animmmals.dequeue('dog'))
+    print(animmmals.dequeue('cat'))
+    print(animmmals.dequeue('snake'))
